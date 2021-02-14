cece941ccdede21829f8b925c194105a4526e778
Bethelhem Teshibelay <se.bethelhem.teshibelay@gmail.com>
2021-02-14 19:54:56 +0300

    timer

--- AsyncDemo/asset/js/timer.js
+++ AsyncDemo/asset/js/timer.js
@@ -2,9 +2,23 @@
 const timerDemo = document.getElementById("timerDemo");
 //timer 
 function startTime() {
-
-}
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    let am_pm = h > 12?'PM':'AM';
+    
+    //convert the hours into 12 format
+    h = h% 12 || 12;
+    m = addZero(m);
+    s = addZero(s);
+    //console.log(se);
+    timerDemo.innerHTML = '${h} : ${addZero(m)} : ${addZero(s)} ${am_pm}';
+    
+}setTimeout(startTime,500);
 
 function addZero(i) {
-
+    if(i < 10){i = "0"+ i;} 
+    return i;
 }
+
