2d6611bee72750fd5c9dcbd8a5036b833fdf6d12
Bethelhem Teshibelay <se.bethelhem.teshibelay@gmail.com>
2021-02-15 02:52:01 +0300

    added loadposts function

--- AsyncDemo/asset/js/loadPosts.js
+++ AsyncDemo/asset/js/loadPosts.js
@@ -5,6 +5,32 @@ const postDiv2 = document.getElementById('myPosts');
 
 //load multiple posts function 
 function loadPosts() {
+    const xhr= new XMLHttpRequest();
+    xhr.open('GET','/AsyncDemo/asset/jsonData/posts.json','true');
+    xhr.onload = function(){
+    if(this.status === 200){
+        const posts = JSON.parse(this.responseText);
+        let output = '';
+    posts.forEach(post =>{
+        output += `
     
+        <div class="item">
+            <div class="image"> <img src="${post.image}"> </div>
+            <div class="content">
+                <a class="header" href="#" id="bTitle"> ${post.postTitle} </a>
+                <div class="meta">
+                    <span id="bDate">${post.date} </span>
+                    <span>By: <a href="#" id="bAuthor"> ${post.name}</a></span>
+                </div>
+                <div class="description">  <p id="bDesc">  ${post.postText} </p  </div>
+                <div class="extra"> <a class="ui floated basic violet button" href="#">Read Mores</a> </div>
+            </div>
+        </div>`;
+        console.log(output);   
+});
+    postDiv2.innerHTML = output;
+    }
+    }
+    xhr.send();
 
 }
--- AsyncDemo/asset/js/loadPostsAPI.js
+++ AsyncDemo/asset/js/loadPostsAPI.js
@@ -10,6 +10,33 @@ document.addEventListener("DOMContentLoaded", () => {
 
 //load post from fake api function 
 function load_fromPlaceHolder() {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(function(res){
+      return res.json();
+  })
+  .then (function(posts){
+      let output = '';
+      posts.forEach(function(post){
+          output += `<div class="item">
+          <div class="image"> <img src="${post.image}"> </div>
+          <div class="content">
+              <a class="header" href="#" id="bTitle"> ${post.postTitle} </a>
+              <div class="meta">
+                  <span id="bDate">${post.date} </span>
+                  <span>By: <a href="#" id="bAuthor"> ${post.name}</a></span>
+              </div>
+              <div class="description">  <p id="bDesc">  ${post.postText} </p  </div>
+              <div class="extra"> <a class="ui floated basic violet button" href="#">Read Mores</a> </div>
+          </div>
+      </div>`;
+
+
+      });
+      postDiv3.innerHTML = output;
+  }) 
+  .catch(function (err) {
+      console.log(err);
+  });
 
 
 }
