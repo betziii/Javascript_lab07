b627682ea3b2b0de28515f4659faf1aab03b0a3a
Bethelhem Teshibelay <se.bethelhem.teshibelay@gmail.com>
2021-02-18 06:03:46 +0300

    bug fix #1

--- AsyncDemo/asset/js/loadPostsAPI.js
+++ AsyncDemo/asset/js/loadPostsAPI.js
@@ -1,12 +1,63 @@
 // UI Vars 
 const postDiv3 = document.getElementById('thePosts');
 const load = document.querySelector('#loadd');
+const filter = document.querySelector('#filter');
 //Load Every thing ....
 document.addEventListener("DOMContentLoaded", () => {
     //load_fromPlaceHolder();
     loadDataNew();
 });
+filter.addEventListener('keyup', filterTasks);
+function filterTasks() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(function(res) {
+        return res.json(); //return the JSON Promise
+    })
+    .then(function(posts) {
+        //iterate over each post [100 posts]
+        let searchArray = Array();
+        posts.forEach(function(post) {
+            searchArray.push({title:post.title.toUpperCase(), body: post.body});
+        });
+        let output = '';
+        for (let i = 0; i < searchArray.length; i++) {
+            const userInput = filter.value.toUpperCase();
+            
+            if (searchArray[i].title.toUpperCase().indexOf(userInput) > -1) {
+                
+        
+            output += `
+    
+            <div class="item">
+            <div class="image">
+                <img src=" https://images.unsplash.com/photo-1499482125586-91609c0b5fd4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80">
+            </div>
+            <div class="content">
+                <a class="header" href="#" id="bTitle">
+                ${searchArray[i].title.toUpperCase()}
+                </a>
+                <div class="description">
+                    <p id="bDesc">
+                    ${searchArray[i].body}
+                    </p>
+                </div>
+                <div class="extra">
+                    <a class="ui floated basic violet button" href="#">Read Mores</a>
+                </div>
+            </div>
+        </div>`;
+            }
+            
+        }
 
+        postDiv3.innerHTML = output;
+     
+    })
+    .catch(function(err) {
+        console.log(err);
+    });
+
+}
 
 //load post from fake api function 
 function load_fromPlaceHolder() {
